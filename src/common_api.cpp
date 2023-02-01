4fd2e26c4e480ceb78931630dab115808b2a6bd7
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-01 13:34:23 -0300

    feat: adding a implementation of api

--- src/common_api.cpp
+++ src/common_api.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
-void print_message(std::string message){
+void print_message_break(std::string message){
   std::cout << message << std::endl;
+}
+
+void print_message(std::string message){
+  std::cout << message;
+}
+void break_line(){
+  std::cout << std::endl;
 }
