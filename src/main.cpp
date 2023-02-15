a2c053ff83826c72003b64cb91f47fec834736e8
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-15 19:26:12 -0300

    [Feat]: Adding a simple tui to this project

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,27 @@
 #include "enterprise.hpp"
 #include "iostream"
 
+void menu(){
+  char input;
+  while (input != 'q'){
+    print_message_break("Trabalho Prático de Algoritmos em Grafos");
+    break_line();
+
+    print_message_break("i -> Inserir elementos");
+    print_message_break("q -> Sair");
+    std::cin >> input;
+
+    switch (input) {
+      case 'i':
+        break;
+      case 'q':
+        break;
+    }
+  };
+}
+
 int main (int argc, char *argv[])
 {
-  
+  menu(); 
   return 0;
 }
